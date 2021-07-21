3c0e788a89f42781f43140c2982e2f57105f55d7
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-21 12:54:25 -0400

    added new components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react'
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path="/ItemLookup" component={ItemLookup} />
+        <Route exact path="/Orders" component={Orders} />
+        <Route exact path="/About" component={About} />
+      </div>
+    </Router>
   );
 }
 
